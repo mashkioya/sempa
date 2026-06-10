e81af905d320db22f92f954cf5f8fa7c1f28709e
mashkioya <5342078+mashkioya@users.noreply.github.com>
2026-06-09 18:05:35 -0600

    added i18n

---
+++ constants/i18n.ts
@@ -0,0 +1,31 @@
+import { getLocales } from 'expo-localization';
+import { I18n } from 'i18n-js';
+
+const translations = {
+    es: {
+        appName: 'Sempa',
+        addFile: 'Agrega un archivo',
+        urlPlaceholder: 'URL',
+        add: 'Agregar',
+        downloading: 'Descargando...',
+        downloadComplete: 'Descarga completa',
+    },
+    en: {
+        appName: 'Sempa',
+        addFile: 'Add file',
+        urlPlaceholder: 'URL',
+        add: 'Add',
+        downloading: 'Downloading...',
+        downloadComplete: 'Download complete',
+    },
+};
+
+const i18n = new I18n(translations);
+const supportedLocales = ['en', 'es'];
+const deviceLocales = getLocales();
+const match = deviceLocales.find(locale => 
+    supportedLocales.includes(locale.languageCode ?? '')
+);
+i18n.enableFallback = true;
+
+export default i18n;
