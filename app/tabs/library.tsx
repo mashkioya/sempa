2815c260084ed1901b6704a7250f48297091d16a
mashkioya <5342078+mashkioya@users.noreply.github.com>
2026-06-09 21:03:15 -0600

    cleaned up styles

--- app/tabs/library.tsx
+++ app/tabs/library.tsx
@@ -32,29 +32,5 @@ const styles = StyleSheet.create({
   main: {
     flex: 2,
     alignItems: 'center',
-  },
-  subtitle: {
-    fontSize: 20,
-  },
-  input: {
-    width: '80%',
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 8,
-    padding: 12,
-    marginTop: 20,
-    fontSize: 15,
-  },
-  button: {
-    backgroundColor: '#007AFF',
-    padding: 12,
-    borderRadius: 8,
-    marginTop: 12,
-    width: '80%',
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 15,
   }
 });
